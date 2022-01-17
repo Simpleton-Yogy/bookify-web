a6fd019e17e7817312974e889cef1a0dc8ac2d16
Simpleton-Yogy <jiri.s.15243@gmail.com>
2022-01-17 21:16:53 +0100

    created basic structure and menu buttons

---
+++ src/App.vue
@@ -0,0 +1,108 @@
+<template>
+  <div class="mainContainer">
+    <div class="menuContainer">
+      <div class="menuLogoContainer">
+        <div class="menuLogoText">Bookify</div>
+      </div>
+      <div class="menuItemsContainer">
+        <MenuItem text = "Home" icon = "homeIcon"  />
+        <MenuItem text = "Library" icon = "libraryIcon" />
+        <MenuItem text = "Search" icon = "searchIcon" />
+        <MenuItem text = "Settings" icon = "settingsIcon" />
+      </div>
+    </div>
+    <div class="contentContainer">
+
+    </div>
+  </div>
+</template>
+
+<script>
+
+// Import Pages
+// import Home from '@/pages/Home.vue';
+// import Library from '@/pages/Library.vue';
+// import Search from '@/pages/Search.vue';
+// import Settings from '@/pages/Settings.vue';
+
+// Import Components
+import MenuItem from '@/components/MenuItem.vue';
+
+
+export default {
+  name: 'App',
+  components: {
+    MenuItem
+  },
+}
+</script>
+
+<style lang="scss">
+
+@import url('https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;1,100;1,200;1,300;1,400;1,500;1,600;1,700&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Lobster&display=swap');
+@import "@/assets/colors.scss";
+
+#app {
+  font-family: 'Montserrat', sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: $text;
+}
+
+body {
+  margin: 0;
+  padding: 0;
+}
+
+.mainContainer {
+  width: 100vw;
+  min-height: 100vh;
+  display: flex;
+  flex-flow: row nowrap;
+  justify-content: stretch;
+  margin: 0;
+  padding: 0;
+
+  .menuContainer {
+    width: 15%;
+    display: flex;
+    flex-flow: column nowrap;
+    justify-content: flex-start;
+    align-items: stretch;
+    background-color: $dark;
+
+    .menuLogoContainer {
+      display: flex;
+      flex-flow: column nowrap;
+      align-items: center;
+      justify-content: stretch;
+
+      .menuLogoText {
+        font-size: 42px;
+        font-family: 'Lobster', cursive;
+        margin: 20px 0;
+      }
+    }
+
+    .menuItemsContainer {
+      display: flex;
+      flex-flow: column nowrap;
+      justify-content: center;
+      align-items: center;
+      background-color: $dark;
+      padding: 0 40px 0 0;
+    }
+  }
+
+  .contentContainer {
+    width: 85%;
+    display: flex;
+    flex-flow: column nowrap;
+    justify-content: flex-start;
+    align-items: flex-end;
+    background-color: $light;
+  }
+}
+
+</style>
---
+++ src/components/MenuItem.vue
@@ -0,0 +1,80 @@
+<template>
+    <div class="menuItemContainer">
+        <input type="radio" class="menuItemFunctional" name = "menu" />
+        <div class="menuItemDummy">
+            <img class="menuItemIcon" :src = "require(`@/assets/${icon}.svg`)" />
+            <div class="menuItemText">{{ text }}</div>
+        </div>
+    </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'MenuItem',
+  components: {
+    
+  },
+  props: {
+      'text': String,
+      'icon': String
+  }
+}
+</script>
+
+<style lang="scss">
+
+@import url('https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;1,100;1,200;1,300;1,400;1,500;1,600;1,700&display=swap');
+@import "@/assets/colors.scss";
+
+.menuItemContainer{
+    width: 100%;
+    display: flex;
+    flex-flow: column nowrap;
+    align-items: center;
+    justify-content: center;
+    margin: 10px 0 20px 0;
+    
+    .menuItemFunctional {
+        width: 100%;
+        height: 42px;
+        margin: 0 0 -42px 0;
+        z-index: 1;
+        opacity: 0;
+        cursor: pointer;
+
+        &:checked + .menuItemDummy {
+            background-color: $blue;
+        }
+    }
+
+    .menuItemDummy {
+        width: 100%;
+        height: 100%;
+        z-index: 0;
+        display: flex;
+        flex-flow: row nowrap;
+        justify-content: flex-start;
+        align-items: center;
+        padding: 8px 0;
+        border-radius: 0 5px 5px 0;
+        background-color: $light;
+        transition: all 250ms ease-in-out;
+
+        .menuItemIcon {
+            height: 20px;
+            width: auto;
+            margin: 0 10px 0 40px;
+        }
+
+        .menuItemText {
+            color: $text;
+            font-family: 'Montserrat', sans-serif;
+            font-size: 18px;
+            font-weight: 600;
+        }
+    }
+}
+
+</style>
